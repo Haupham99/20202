d789dc9b387f7e5690a39ebbc02b9c20767303da
Haupham99 <haupham99a@gmail.com>
2021-04-20 09:33:43 +0700

    up

--- source/models/user.model.js
+++ source/models/user.model.js
@@ -9,7 +9,7 @@ let UserSchema = new Schema({
     username: String,
     birthday: String,
     avatar: {type: String, default: "default.jpg"},
-    class: String,
+    class: {type: String, default: "KHMT-02-K62"},
     // gender: {type: String, default: "male"},
     phone: {type: Number, default: null},
     // address: {type: String, default: "avatar-default.jpg"},
